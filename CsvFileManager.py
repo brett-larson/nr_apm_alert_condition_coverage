05240e035abc0cccd8a7dddd83df940130dadffe
Brett Larson <larson.brett.s@gmail.com>
2023-09-25 10:59:23 -0400

    Update CsvFileManager.py
    Remove unused csv writer logic.

--- CsvFileManager.py
+++ CsvFileManager.py
@@ -11,29 +11,6 @@ class CsvFileManager:
         # self.csv_directory = 'csv_files'
         # self.csv_files_directory = os.path.join(self.parent_directory, self.csv_directory, self.file_name)
 
-    def write_csv(self, data):
-
-        try:
-            # fieldnames = list(data[0].keys())
-            with open(self.file_name, mode='w', newline='') as file:
-                writer = csv.writer(file)
-                for row in data:
-                    print(row)
-                    #writer.writerow(row)
-                file.close()
-        except FileNotFoundError:
-            print(f"File not found: {self.file_name}. Exiting the application.")
-            exit()
-        except PermissionError:
-            print(f"Permission denied: {self.file_name}. Exiting the application.")
-            exit()
-        except IOError as e:
-            print(f"IOError: {e}. Exiting the application.")
-            exit()
-        except Exception as e:
-            print(f"Exception: {e}. Exiting the application.")
-            exit()
-
     def write_dicts_to_csv(self, list_of_dicts):
 
         headers = ['name', 'guid', 'alert_severity', 'reporting']
